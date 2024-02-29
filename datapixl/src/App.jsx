1d4a12ecc87d0b3dd0403287705b5733f20af8f5
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2024-03-01 01:52:16 +0530

    setup completed

---
+++ datapixl/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-black text-white text-3xl text-center">
+          hello this is datapixl
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
