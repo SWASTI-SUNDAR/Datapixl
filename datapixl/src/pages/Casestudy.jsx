26e56f132a10f5e9127cc6b8f125a4c166bed8be
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2024-03-07 12:41:46 +0530

    all bug fixed

--- datapixl/src/pages/Casestudy.jsx
+++ datapixl/src/pages/Casestudy.jsx
@@ -12,6 +12,8 @@ const Casestudy = () => {
       left: 0,
       behavior: "smooth",
     });
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
   };
   return (
     <section id="projects">
